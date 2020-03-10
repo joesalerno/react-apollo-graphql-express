615fb04714d32ee2cb410986f3dd4f07ea14c0df
Joe Salerno <joesalerno.js@gmail.com>
2020-03-10 00:43:47 -0400

    added FeatureView component

---
+++ frontend/src/components/FeatureView.js
@@ -0,0 +1,262 @@
+import React, { useRef, useEffect } from "react"
+import validOdbSymbol from "../modules/validOdbSymbol"
+const degreeToRadian = degree => degree * Math.PI / 180
+
+
+
+export default ({heightPx, widthPx, features}) => {
+  const canvasRef = useRef(null)
+
+  const drawPad = ({ symbol, x, y, polarity, rotation, resize, fillStyle }) => {
+    const ctx = canvasRef.current.getContext("2d")
+    const type = validOdbSymbol(symbol)
+    if (!type) return console.log({ParsingError: {symbol, x, y, polarity, rotation, resize}})
+
+    const getParams = () => symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+
+    const rotate = ({x, y, degrees}) => {
+      ctx.translate(x,y)
+      ctx.rotate(degreeToRadian(degrees))
+      ctx.translate(-x,-y)
+    }
+
+    const square = ({x, y, s}) => ctx.rect(x-s/2, y-s/2, s, s)
+    const rectangle = ({x, y, w, h}) => ctx.rect(x-w/2, y-h/2, w, h)
+    const circle = ({x, y, r}) => ctx.arc(x, y, r, 0, 2*Math.PI)
+    const roundedRectangle = ({x, y, w, h, rad, corners}) => {
+      if (w < 2 * rad) rad = w / 2 //fix rad > h*2 or w*2
+      if (h < 2 * rad) rad = h / 2
+      const halfW = w / 2; const halfH = h / 2
+      ctx.moveTo(x+halfW, y+halfH)
+      ctx.arcTo(x-halfW, y+halfH, x-halfW, y-halfH, rad)
+      ctx.arcTo(x-halfW, y-halfH, x+halfW, y-halfH, rad)
+      ctx.arcTo(x+halfW, y-halfH, x+halfW, y+halfH, rad)
+      ctx.arcTo(x+halfW, y+halfH, x-halfW, y+halfH, rad)
+    }
+    const chamferedRectangle = ({x, y, w, h, rad, corners}) => {
+      if (w < 2 * rad) rad = w / 2 //fix rad > h*2 or w*2
+      if (h < 2 * rad) rad = h / 2
+      const halfW = w / 2; const halfH = h / 2
+      ctx.moveTo(x+halfW-rad, +y+halfH)
+      ctx.lineTo(x+halfW,     y+halfH-rad)
+      ctx.lineTo(x+halfW,     y-halfH+rad)
+      ctx.lineTo(x+halfW-rad, y-halfH)
+      ctx.lineTo(x-halfW+rad, y-halfH)
+      ctx.lineTo(x-halfW,     y-halfH+rad)
+      ctx.lineTo(x-halfW,     y+halfH-rad)
+      ctx.lineTo(x-halfW+rad, y+halfH)
+      ctx.lineTo(x+halfW+rad, y+halfH)
+    }
+    const diamond = ({x, y, w, h}) => {
+      const halfW = w / 2; const halfH = h / 2
+      ctx.moveTo(x-halfW, y)
+      ctx.lineTo(x, y+halfH)
+      ctx.lineTo(x+halfW, y)
+      ctx.lineTo(x, y-halfH)
+      ctx.lineTo(x-halfW, y)
+    }
+    const horzHexagon = ({x, y, w, h, r}) => {
+      const halfW = w / 2; const halfH = h / 2
+      ctx.moveTo(x+halfW, y)
+      ctx.lineTo(x+halfW-r, y-halfH)
+      ctx.lineTo(x-halfW+r, y-halfH)
+      ctx.lineTo(x-halfW, y)
+      ctx.lineTo(x-halfW+r, y+halfH)
+      ctx.lineTo(x+halfW-r, y+halfH)
+      ctx.lineTo(x+halfW, y)
+    }
+    const vertHexagon = ({x, y, w, h, r}) => {
+      const halfW = w / 2; const halfH = h / 2
+      ctx.moveTo(x, y+halfH)
+      ctx.lineTo(x+halfW, y+halfH-r)
+      ctx.lineTo(x+halfW, y-halfH+r)
+      ctx.lineTo(x, y-halfH)
+      ctx.lineTo(x-halfW, y-halfH+r)
+      ctx.lineTo(x-halfW, y+halfH-r)
+      ctx.lineTo(x, y+halfH)
+    }
+    const butterfly = ({x, y, d}) => {
+      //ctx.arc(x, y, d/2, 0, 2*Math.PI)
+      const r = d / 2
+      ctx.moveTo(x, y+r)
+      ctx.lineTo(x, y-r)
+      ctx.arcTo(x+r, y-r, x+r, y, r)
+      ctx.lineTo(x-r, y)
+      ctx.arcTo(x-r, y+r, x, y+r, r)
+    }
+    const sqrButterfly = ({x, y, s}) => {
+      const halfS = s / 2
+      square({x: x+halfS/2, y: y-halfS/2, s: halfS})
+      square({x: x-halfS/2, y: y+halfS/2, s: halfS})
+    }
+    const triangle = ({x, y, base, h}) => {
+      const halfBase = base/2; const halfH = h/2
+      ctx.moveTo(x+halfBase, y-halfH)
+      ctx.lineTo(x-halfBase, y-halfH)
+      ctx.lineTo(x, y+halfH)
+      ctx.lineTo(x+halfBase, y-halfH)
+    }
+    const halfOval = ({x, y, w, h}) => {
+      // todo implement when look into crazy way odb specifies corners
+    }
+    const roundedRoundThermal = ({x, y, od, id, angle, num_spokes, gap}) => {
+      const gap = od-id || 0
+      
+    }
+
+    ctx.beginPath()
+    ctx.fillStyle = fillStyle
+
+    ctx.save()
+    let [ sym_rotation] = symbol.match(/_(3[0-5][0-9]|[12][0-9][0-9]|[1-9]?[0-9])$/g) || [ false ]
+    if (sym_rotation) {
+      sym_rotation = sym_rotation.substr(1, sym_rotation.length)
+      rotate({x, y, degrees: sym_rotation})
+    }
+    if (rotation) rotate({x, y, degrees: rotation})
+
+    if (type === "square") {
+      const [ s ] = getParams()
+      square({x, y, s})
+    }
+
+    else if (type === "round") {
+      const [ d ] = getParams()
+      circle({x, y, r: d/2})
+    }
+
+    else if (type === "rectangle") {
+      const [ w, h ] = getParams()
+      rectangle({x, y, w, h})
+    }
+
+    else if (type === "rounded rectangle") {
+      const [ w, h, rad, corners ] = getParams()
+      roundedRectangle({x, y, w, h, rad, corners})
+    }
+
+    else if (type === "chamfered rectangle" || type ==="octagon") {
+      const [ w, h, rad, corners ] = getParams()
+      chamferedRectangle({x, y, w, h, rad, corners})
+    }
+
+    else if (type === "oval") {
+      const [ w, h ] = getParams()
+      roundedRectangle({x, y, w, h, rad: Math.min(w/2, h/2)})
+      /* Below draws ellipse, they mean a slot */
+      // const [ w, h, rotation ] = getParams()
+      // ctx.ellipse(x, y, w, h, degreeToRadian(rotation) || 0, 0, 2*Math.PI)
+      // ctx.fill()
+    }
+
+    else if (type === "diamond") {
+      const [ w, h ] = getParams()
+      diamond({x, y, w, h})
+    }
+
+    else if (type === "octagon") {
+      const [ w, h, rad ] = getParams()
+      chamferedRectangle({x, y, w, h, rad})
+    }
+
+    else if (type === "round donut") {
+      const [ od, id ] = getParams()
+      circle({x, y, r: od/2})
+      circle({x, y, r: id/2})
+    }
+
+    else if (type === "square donut") {
+      const [ od, id ] = getParams()
+      square({x, y, s: od})
+      square({x, y, s: id})
+    }
+
+    else if (type === "square/round donut") {
+      const [ od, id ] = getParams()
+      square({x, y, s: od})
+      circle({x, y, r: id/2})
+    }
+
+    else if (type === "rounded square donut") {
+      const [ od, id, rad, corners] = getParams()
+      roundedRectangle({x, y, w: od, h: od, rad, corners})
+      roundedRectangle({x, y, w: id, h: id, rad: Math.max(rad-(od-id)/2,  0), corners})
+    }
+    
+    else if (type === "rectangle donut") {
+      const [ ow, oh, lw ] = getParams()
+      rectangle({x, y, w: ow, h: oh})
+      rectangle({x, y, w: ow-lw*2, h: oh-lw*2})
+    }
+
+    else if (type === "rounded rectangle donut") {
+      const [ ow, oh, lw, rad, corners ] = getParams()
+      roundedRectangle({x, y, w: ow, h: oh, rad, corners})
+      roundedRectangle({x, y, w: ow-lw*2, h: oh-lw*2, rad: Math.max(rad-(lw), 0), corners})
+    }
+
+    else if (type === "oval donut") {
+      const [ ow, oh, lw ] = getParams()
+      roundedRectangle({x, y, w: ow, h: oh, rad: Math.min(ow/2, oh/2)})
+      roundedRectangle({x, y, w: ow-lw*2, h: oh-lw*2, rad: Math.max(Math.min((ow-lw*2)/2, (oh-lw*2)/2), 0)})
+    }
+
+    else if (type === "horizontal hexagon") {
+      const [ w, h, r ] = getParams()
+      horzHexagon({x, y, w, h, r})
+    }
+
+    else if (type === "vertical hexagon") {
+      const [ w, h, r ] = getParams()
+      vertHexagon({x, y, w, h, r})
+    }
+
+    else if (type === "butterfly") {
+      const [ d ] = getParams()
+      butterfly({x, y, d})
+    }
+
+    else if (type === "square butterfly") {
+      const [ s ] = getParams()
+      sqrButterfly({x, y, s})
+    }
+
+    else if (type === "triangle") {
+      const [ base, h ] = getParams()
+      triangle({x, y, base, h})
+    }
+
+    else if (type === "round thermal (rounded)") {
+      const [ od, id, angle, num_spokes, gap ] = getParams()
+      roundedRoundThermal({x, y, od, id, angle, num_spokes, gap})
+    }
+
+    ctx.fill("evenodd")
+    ctx.restore()
+  }
+
+  const draw = () => {
+    const ctx = canvasRef.current.getContext("2d")
+
+    //const canvas = canvasRef.current
+    //const ctx = canvas.getContext("2d")
+
+    ctx.fillStyle = "black"
+    ctx.rect(0,0,widthPx,heightPx)
+    ctx.fill()
+    ctx.closePath()
+
+    for(const feature of features) {
+      console.log(feature)
+      drawPad({...feature, fillStyle:"yellow"})
+    }
+  }
+
+  useEffect(draw, [heightPx, widthPx, features])
+
+  return <canvas onClick={draw} ref={canvasRef} height={`${heightPx}px`} width={`${widthPx}px`} style={{
+    border: "1px solid", height: `${heightPx}px`, width:`${widthPx}px`
+  }}>
+    Sorry, your browser doesn't support the canvas tag.
+  </canvas>
+}
--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -3,6 +3,7 @@ import WindowLayout from "../components/WindowLayout"
 import Spinner from "../components/Spinner"
 import Dropdown from "../components/Dropdown"
 import CrossSection from "../components/CrossSection"
+import FeatureView from "../components/FeatureView"
 import validOdbSymbol from "../modules/validOdbSymbol"
 
 const degreeToRadian = degree => degree * Math.PI / 180
@@ -13,7 +14,7 @@ const TestPage = ({auth, login, logout}) => {
   const getData = () => {let d = [];for (let i = 0; i <= 250; i++) d.push({symbol:`r${i}`}); return d}
   useEffect(() => setInputSelectedPad(selectedPad ? selectedPad.symbol : ""), [selectedPad])
 
-  const features = [{type: "r", }]
+  // const features = [{type: "r", }]
 
   const pathFromSymbol = symbol => {
     let type = validOdbSymbol(symbol)
@@ -22,11 +23,9 @@ const TestPage = ({auth, login, logout}) => {
     if (!type) return path
     if (type === "round") {
       path.arc(0, 0, )
-      let [d, r] = symbol.match(/[0-9]+/g)
+      let [d, r] = symbol.match(/[0-9]+/g).map(text => parseFloat(text))
       console.log({symbol, d, r})
 
-      
-      
       //ctx.arc(100, 75, 50, 0, 2 * Math.PI)
 
       // return [{type: "arc", x:0, y:0, }]
@@ -47,7 +46,7 @@ const TestPage = ({auth, login, logout}) => {
     ctx.fillStyle = "yellow"
 
     if (type === "square") {
-      let [s, rotation] = symbol.match(/[0-9]+(\.[0-9]*)?/g)
+      let [s, rotation] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
       ctx.save()
       if (rotation) {
         ctx.translate(x+s/2,y+s/2)
@@ -60,13 +59,13 @@ const TestPage = ({auth, login, logout}) => {
     }
 
     else if (type === "round") {
-      let [d] = symbol.match(/[0-9]+(\.[0-9]*)?/)
-      ctx.arc(x, y, d/2, 0, Math.PI*2)
+      let [d] = symbol.match(/[0-9]+(\.[0-9]*)?/).map(text => parseFloat(text))
+      ctx.arc(x, y, d/2, 0, 2*Math.PI)
       ctx.fill("evenodd")
     }
 
     else if (type === "rectangle") {
-      let [w, h, rotation] = symbol.match(/[0-9]+(\.[0-9]*)?/g)
+      let [w, h, rotation] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
       ctx.save()
       if (rotation) {
         ctx.translate(x+w/2,y+h/2)
@@ -78,14 +77,14 @@ const TestPage = ({auth, login, logout}) => {
       ctx.restore()
     }
 
-    else if (type = "rounded rectangle") {
-      let [sym, rotation] = symbol.split("_")
-      let [w, h, rad, corners] = sym.match(/[0-9]+(\.[0-9]*)?/g)
-      if (w < 2 * rad) rad = w / 2
+    else if (type === "rounded rectangle" || type ==="oval") {
+      let [ , rotation] = symbol.split("_").map(text => parseFloat(text))
+      let [w, h, rad, corners] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      if (type === "oval") rad = Math.min(w/2, h/2)
+      if (w < 2 * rad) rad = w / 2 //fix rad > h*2 or w*2
       if (h < 2 * rad) rad = h / 2
       ctx.save()
-      let halfW = w / 2
-      let halfH = h / 2
+      let halfW = w / 2; let halfH = h / 2
       if (rotation) {
         ctx.translate(x, y)
         ctx.rotate(degreeToRadian(rotation))
@@ -100,11 +99,107 @@ const TestPage = ({auth, login, logout}) => {
       ctx.restore()
     }
 
+    else if (type === "chamfered rectangle" || type ==="octagon") {
+      let [ , rotation] = symbol.split("_").map(text => parseFloat(text))
+      let [w, h, rad, corners] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      if (w < 2 * rad) rad = w / 2 //fix rad > h*2 or w*2
+      if (h < 2 * rad) rad = h / 2
+      ctx.save()
+      let halfW = w / 2; let halfH = h / 2
+      if (rotation) {
+        ctx.translate(x, y)
+        ctx.rotate(degreeToRadian(rotation))
+        ctx.translate(-x, -y)
+      }
+      ctx.moveTo(x+halfW-rad, +y+halfH)
+      ctx.lineTo(x+halfW,     y+halfH-rad)
+      ctx.lineTo(x+halfW,     y-halfH+rad)
+      ctx.lineTo(x+halfW-rad, y-halfH)
+      ctx.lineTo(x-halfW+rad, y-halfH)
+      ctx.lineTo(x-halfW,     y-halfH+rad)
+      ctx.lineTo(x-halfW,     y+halfH-rad)
+      ctx.lineTo(x-halfW+rad, y+halfH)
+      ctx.lineTo(x+halfW+rad, y+halfH)
+      ctx.fill()
+      ctx.restore()
+    }
+
+    else if (type === "oval") { /* Draw a rounded rectangle */
+      /* Below is wrong, draws oval, they mean a slot */
+      // let [ w, h, rotation ] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      // ctx.ellipse(x, y, w, h, degreeToRadian(rotation) || 0, 0, 2*Math.PI)
+      // ctx.fill()
+    }
+
+    else if (type === "diamond") {
+      let [ w, h, rotation ] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      let halfW = w / 2; let halfH = h / 2
+      ctx.save()
+      if (rotation) {
+        ctx.translate(x, y)
+        ctx.rotate(degreeToRadian(rotation))
+        ctx.translate(-x, -y)
+      }
+      ctx.moveTo(x-halfW, y)
+      ctx.lineTo(x, y+halfH)
+      ctx.lineTo(x+halfW, y)
+      ctx.lineTo(x, y-halfH)
+      ctx.lineTo(x-halfW, y)
+      ctx.fill()
+      ctx.restore()
+    }
+
+    else if (type === "octagon") {/* draw a chamfered rectangle */}
+
+    else if (type === "round donut") {
+      let [ od, id, rotation ] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      ctx.arc(x, y, od/2, 0, 2*Math.PI)
+      ctx.arc(x, y, id/2, 0, 2*Math.PI)
+      ctx.fill("evenodd")
+    }
+
+    else if (type === "square donut") {
+      let [ od, id, rotation ] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      let halfOd = od / 2
+      let halfId = id / 2
+      ctx.save()
+      if (rotation) {
+        ctx.translate(x, y)
+        ctx.rotate(degreeToRadian(rotation))
+        ctx.translate(-x, -y)
+      }
+      ctx.rect(x-halfOd, y-halfOd, od, od)
+      ctx.rect(x-halfId, y-halfId, id, id)
+      ctx.fill("evenodd")
+      ctx.restore()
+    }
+
+    else if (type === "square/round donut") {
+      let [ od, id, rotation ] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+      let halfOd = od / 2
+      ctx.save()
+      if (rotation) {
+        ctx.translate(x, y)
+        ctx.rotate(degreeToRadian(rotation))
+        ctx.translate(-x, -y)
+      }
+      ctx.rect(x-halfOd, y-halfOd, od, od)
+      ctx.arc(x, y, id/2, 0, 2*Math.PI)
+      ctx.fill("evenodd")
+      ctx.restore()
+    }
+
+    else if (type === "rounded square donut") {
+      let [ , rotation] = symbol.split("_").map(text => parseFloat(text))
+      let [od, id, rad, corners] = symbol.match(/[0-9]+(\.[0-9]*)?/g).map(text => parseFloat(text))
+
+    }
+
   }
 
+  const draw = () => {}
 
-
-  const draw = features => {
+  const _draw = features => {
     let canvas = document.getElementById("canvas")
     let ctx = canvas.getContext("2d")
     
@@ -133,10 +228,41 @@ const TestPage = ({auth, login, logout}) => {
     drawPad({symbol: "s30", x:365, y:365})
     drawPad({symbol: "rect100x20xr21_45", x:150, y:200})
     drawPad({symbol: "rect100x20xr21", x:150, y:200})
-    //degree * Math.PI / 180 degree -> radian
+    drawPad({symbol: "rect100x20xc5", x:350, y:200})
+    drawPad({symbol: "oval25x45_45", x:350, y:100})
+    drawPad({symbol: "di45x45", x:350, y:250})
+    drawPad({symbol: "oct25x25x5", x:350, y:300})
+    drawPad({symbol: "donut_r25x15", x:300, y:300})
+    drawPad({symbol: "donut_s25x15", x:260, y:300})
+    drawPad({symbol: "donut_sr25x15", x:220, y:300})
   }
 
+  const features = [
+    {symbol: "s10_5", x:5, y:5},
+    {symbol: "s20_45", x:35, y:35},
+    {symbol: "s30_5", x:65, y:65},
+    {symbol: "s30", x:365, y:365},
+    {symbol: "rect100x20xr21_45", x:150, y:200},
+    {symbol: "rect100x20xr21", x:150, y:200},
+    {symbol: "rect100x20xc5", x:350, y:200},
+    {symbol: "oval25x45_45", x:350, y:100},
+    {symbol: "di45x45", x:350, y:250},
+    {symbol: "oct25x25x5", x:350, y:300},
+    {symbol: "donut_r25x15", x:300, y:300},
+    {symbol: "donut_s25x15", x:260, y:300},
+    {symbol: "donut_sr25x15", x:220, y:300},
+    {symbol: "donut_s100x80x25", x:160, y:300},
+    {symbol: "donut_rc45x25x7.5", x:160, y:300},
+    {symbol: "donut_rc45x25x7.5x20", x: 80, y:300},
+    {symbol: "donut_o45x25x7.5_15", x: 50, y:300},
+    {symbol: "hex_l45x25x7.5", x: 50, y:350},
+    {symbol: "hex_s25x45x7.5", x: 50, y:250},
+    {symbol: "bfr25", x: 50, y:200},
+    {symbol: "bfs25", x: 50, y:150},
+    {symbol: "tri25x25", x: 50, y:100},
 
+  ]
+  
   const links = [{name:"CAM",ref:"/"}, {name:"Test"}]
   return <WindowLayout title={"Test"} links={links} auth={auth} login={login} logout={logout}>
     <div style={{minHeight:272, width:"100%", display:"flex", flexDirection:"column"}}>
@@ -160,7 +286,7 @@ const TestPage = ({auth, login, logout}) => {
       <CrossSection layers={[["1em","32","yellow"], ["2eg","25","#a6a600"], ["3eg","35","yellow"]]} drillSize={"13"}/>
 
       <canvas id="canvas" height="400px" width="500px" style={{border: "1px solid"}}>
-        Sorry your browser doesn't support the canvas tag.
+        Sorry, your browser doesn't support the canvas tag.
       </canvas>
 
       <button onClick={draw}>draw</button>
@@ -170,6 +296,9 @@ const TestPage = ({auth, login, logout}) => {
       {setTimeout(draw, 200)}
 
       {selectedPad && selectedPad.symbol}
+
+      <FeatureView heightPx={500} widthPx={500} features={features}/>
+
       <Spinner style={{margin:"auto auto 40px auto"}}/>
     </div>
   </WindowLayout>
